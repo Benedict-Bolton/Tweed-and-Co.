7917da78907a54b8d13cca64c1d0b430d37805f6
Benedict Bolton <benedict.bolton@gmail.com>
2014-01-14 20:21:37 -0500

    Super class for Senate, District, and Country; only accessor completed

---
+++ Others.java
@@ -0,0 +1,42 @@
+public class Others {
+    
+    //Instance Vairables
+    
+    //willingness to lose and compromise
+    private double _extremismRight;
+    private double _extremismLeft;
+    
+    //Composition, must add to 100 ALWAYS
+    private double _percentCompRight;
+    private double _percentCompLeft;
+    
+    //Voting Factors
+    private long _averageIncome; //tied to all domestic events
+    private int _hawkLvl; //tied to all foreign events
+    private int _careBearLvl; //tied to all events
+    
+    //Accessor Methods
+    public double getExRight () {
+	return _extremismRight;
+    }
+    public double getExLeft () {
+	return _extremismLeft; 
+    }
+    public double getCompRight () {
+	return _percentCompRight;
+    }
+    public double getCompLeft () {
+	return _percentCompLeft;
+    }
+    public long getAveInc () {
+	return _averageIncome;
+    }
+    public int getHawks () {
+	return _hawkLvl;
+    }
+    public int getBears () {
+	return _careBearLvl;
+    }
+    
+    
+    
