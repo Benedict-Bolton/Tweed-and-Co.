cf553d8a77c4d272626c8db5e40e521023566320
Shahruz Ghaemi <shahruzghaemi@gmail.com>
2014-01-15 14:00:04 -0500

    Senate class and added mutators to Others class

--- Others.java
+++ Others.java
@@ -2,15 +2,16 @@ public class Others {
     
     //Instance Vairables
     
-    //willingness to lose and compromise
+    //willingness to lose and compromise (from 0 to 1, aka from 0% to 100%)
+    //each var is capped at 100%, but can sum to <=200%
     private double _extremismRight;
     private double _extremismLeft;
     
-    //Composition, must add to 100 ALWAYS
+    //Composition, vars must add to 100% ALWAYS
     private double _percentCompRight;
     private double _percentCompLeft;
     
-    //Voting Factors
+    //Voting Factors (all uncapped)
     private long _averageIncome; //tied to all domestic events
     private int _hawkLvl; //tied to all foreign events
     private int _careBearLvl; //tied to all events
@@ -38,6 +39,37 @@ public class Others {
 	return _careBearLvl;
     }
     
+    //Mutators
+        public double setExRight(double input){
+	double old = _extremismRight;
+	_extremismRight = input;
+	return old;}
+    public double setExLeft(double input){
+	double old = _extremismLeft;
+	_extremismLeft = input;
+	return old;}
+    public double setCompRight(double input){
+       	double old = _percentCompRight;
+	_percentCompRight = input;
+	return old;}
+    public double setCompLeft(double input){
+	double old = _percentCompLeft;
+	_percentCompLeft = input;
+	return old;}
+    public long setAveInc(double input){
+	long old = _averageIncome;
+	_averageIncome = input;
+	return old;}
+    public int setBears(int input){
+	int old = _careBearLvl;
+	_careBearLvl = input;
+	return old;}
+    public int setHawks(int input){
+	int old = _hawkLvl;
+	_hawkLvl = input;
+	return old;}
+
+    //METHODS
     public boolean voteLegis (String tactics; Legislation action; Event lastEvent) {
     	//checks depending variables, if they concur, then the bill passes, if not it fails
     	return false;
---
+++ Senate.java
@@ -0,0 +1,24 @@
+public class Senate extends Others{
+    
+    //Instance Vars
+
+
+    //Constructors
+    //default constructor creates a 1996 Senate 
+    //(beginning of Clinton's second term)
+    public Senate(){
+	setExRight(0.0);//cupholder
+	setExLeft(0.0);//cupholder
+	setCompRight(0.55);
+	setCompLeft(0.45);
+	setHawks(45);
+	setBears(40);
+
+	
+    }
+    
+    
+    
+    
+    
+}
