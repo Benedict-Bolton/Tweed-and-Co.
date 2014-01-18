1800615043ff4a4945eacb01e5db11676187a5af
Benedict Bolton <benedict.bolton@gmail.com>
2014-01-18 16:35:16 -0500

    added instance variables

--- Legislation.java
+++ Legislation.java
@@ -6,19 +6,35 @@ public class Legislation {
     private String text;
 
     //all possible bill texts
-    //8 attributes for each bill == text, econType, socType, forType, proposer, changeRelection, PorkBarrel, selfEnrichment;
-    private final int[][] POS_BILLS = { {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} };
-    private final String[] TEXT_REFERENCE;
+    //8 attributes for each bill == text, econType, socType, forType, proposer, PorkBarrel, selfEnrichment;
+    private final int[][] POS_BILLS = { {0, 1, 0, 0, -1, 0, 1}, {1, 0, 0, 0, 0, -1, 2}, {2, 0, 0, 0, 0, -1, 2}, {3, 0, 0, 0, -1, 1, 2}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} };
+    private final String[] TEXT_REFERENCE = new String[25];
+
+    private int propParty; //1 = demo 0 = repub
+    private int porkBarrel; // 1 = yes, 0 = no
+    private int selfEnrichment; // 1 = yes, 0 = no
 
     //types of bills, form of ints which must be -1, 0, 1, or 2
     private int foreignType;
     // 0 = Interventionist, 1 = Internationalist, 2 = Isolationist, -1 = has nothing to do with Foreign Affairs
     private int economicsType;
     // 0 = Populist (Raises jobs, neutral on income, lowers stock market), 1 = Trickle-Down (Raises Jobs and Stock Market, hurts income), 2 = Modernization (Raises Incomes and Stock Market, hurts Jobs, -1 = no effect on economy);
-    // -1 , 0, 1, 2, 3
+    // 0, 1, 2, 3
     private int socialType;
     // 0 = Libertarians (0% regulation) , 1 = Left-Leaning, 2 = Right-Leaning, 3 = Fascist (100% regulation)
     
+    public Legislation () {
+	int randChoice = Math.random() * 25;
+	text = TEXT_REFERENCE[ POS_BILLS[randChoice][0] ];
+	propParty = POS_BILLS[randChoice][1];
+	porkBarrel = POS_BILLS[randChoice][2];
+	selfEnrichment= POS_BILLS[randChoice][3];
+	foreignType= POS_BILLS[randChoice][4];
+	economicsType = POS_BILLS[randChoice][5];
+	socialType = POS_BILLS[randChoice][6];
+	
+    }
+
     //accessors
     public int getSoc () {
 	return socialType;
@@ -47,7 +63,7 @@ public class Legislation {
 	TEXT_REFERENCE[10] = "Enable Greater Investment and Vitality on Wall-Street:\n The New York Stock Exchange is the largest Stock Exchange in the world, and the US hosts the largest financial sector in the world. It generates billions of dollars a year for the US, yet it is constrained by unduly restrictive regulation. We need to liberate the financial markets. Regulations will be dropped in the Financial markets allowing for greater investment and economic growth (exact changes need to be expanded on)";
 	TEXT_REFERENCE[11] =  "Align the US with International Sentiment:\n In order to improve the international image of the United States any aggressive military action carried out against an armed foreign power, in non-emergancy situations (these determined by the Presidant and JCS) now require either the passage of a resolution within the UN general assembly calling for said action, authorization from the UN security council, or a 2/3 vote from the Senate and 3/5 vote from the House.";
 	TEXT_REFERENCE[12] = "Fund Research and Development in New Technologies:\n Funding will be diverted from the budget for military bases and aid abroad towards the funding of new technologies for use by the United States armed forces. These advances shall focuse on new military technologies, as well as new production techniques for US military gear to decrease the man power, materials, and time needed for its production through mechanization and digitization of production methods.";
-	//I'll do three more real bills, you need to write 5 Shahruz, and ill write the 10 corrupt bills
+	//I'll do three more real bills, you need to write 5 Shahruz, and ill write the 5 corrupt bills
 	//from there i will cover the rest of the class, we need to distribute tomorrow what must be done by 3:30 on Sunday
     	
 }
